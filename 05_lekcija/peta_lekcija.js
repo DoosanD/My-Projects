9018adad887a9c6af290add67a37ebe36c1a4348
DoosanD <davicdusan@gmail.com>
2022-02-15 16:59:08 +0100

    14.02

--- 05_lekcija/peta_lekcija.js
+++ 05_lekcija/peta_lekcija.js
@@ -233,3 +233,133 @@ function noviPozitivanNiz(nizBrojeva){
 }
 
 */
+
+
+//14.02.2022
+
+let niz = ['papir', 'olovka', 'knjiga'];
+//console.log(niz.includes('papir'));
+
+let brojeviProvera = [1,3,4,5,6];
+//console.log(brojeviProvera.includes(2));
+
+// Zadatak
+//Potrebno je definisati funkciju koja računa proizvod dva broja i ispisuje vrednost proizvoda.
+
+function proizvod(n, m){
+  return n*m;
+}
+
+
+//Zadatak
+//Definisati funkciju koja prima dva parametra i računa vrednost prvog parametra na drugi parametar.
+function eksponent(n, m){
+  console.log(n**m)
+}
+
+//Zadatak
+//Definisati funkciju koja računa proizvod dva broja a zatim računa 
+//da li je proizvod paran ili ne i korisniku ispisuje poruku. 
+//Poruku je potrebno ispisati uz pomoc funkcije alert()
+function novaFunkcija(a,b){
+  let nasProizvod = proizvod(a,b);
+  if(nasProizvod%2==1){
+    alert("Nas proizvod nije paran");
+  }
+  else{
+    alert("Nas proizvod je paran");
+  }
+}
+
+//Zadatak
+//Napisati funkciju koja uz pomoć for petlje ispisuje brojeve od 1 do 10
+//a nakon toga ispisuje brojeve od 10 do 1
+function ispisi(){
+  for(let i=1; i<=10; i++){
+    console.log(i);
+  }
+  for(let i=10; i>0; i--){
+    console.log(i);
+  }
+}
+//Istu ovu funkciju , samo je potrebno da funkcija prima parametara do kog broji
+function ispisiNova(n){
+  for(let i=1; i<=n; i++){
+    console.log(i);
+  }
+  for(let i=n; i>0; i--){
+    console.log(i);
+  }
+}
+
+
+//Zadatak
+//Napisati funkciju koja uz pomoć for petlje ispisuje 
+//samo parne brojeve od 1 do 100. 
+function odJedanDoSto(){
+  for(let i=1; i<=100; i++){
+    if(i%2==0){
+      console.log(i);
+    }
+  }
+}
+
+
+//Zadatak
+//Deklarisati niz i dodeliti mu nekoliko elemenata niza , 
+//a zatim promeniti sve elemente niza uz pomoć for petlje.
+function promeniNiz(){
+  let niz = [1,2,3,4,5];
+  for(let i=0; i<niz.length; i++){
+    niz[i] = niz[i]+5;
+  }
+  console.log(niz);
+}
+
+
+// forEach petlja
+function zaSvakiElement(){
+  let automobili = ['Audi', 'Fiat', 'Citroen', 'Renault'];
+  automobili.forEach(auto => {console.log(auto)});
+}
+
+// Napisati niz koji se sastoji od muski ili zenskih imena , 
+// zatim unutar funkcije deklarisati vase ime i proveriti da li se nalazi u nizu
+// poruku ispisati uz pomoc alerta
+
+function imenaProvera() {
+  let muskaImena = ["Milos", "Filip", "Marko", "Nikola", "Uros", "Dusan"];
+  let mojeIme = "Filip";
+  let has = false;
+  muskaImena.forEach(ime => {
+    if (ime == mojeIme) {
+      has = true;
+    }
+  })
+  if (has) {
+    alert("Moje ime se nalazi u nizu")
+  }
+  else {
+    alert("Moje ime se ne nalazi u nizu")
+  }
+}
+
+// zadatak za domaci
+// Napisati funkciju koja iz JMBG-a vraća datum rođenja osobe, formatiran dd. mm. yyyy.
+// (ako 5 cifra pocinje sa 9, godinu vratiti sa 1 ispred, a u ostalim slucajevima da vrati 2 ispred)
+// jmbg 1201970123456 vratiti kao 12. 01. 1970.
+// jmbg 1201003123456 vratiti kao 12. 01. 2003.
+
+function format(jmbg) {
+  jmbg = jmbg.toString();
+  let formated;
+  for (let i = 0; i < jmbg.length; i++) {
+    if (jmbg[4] == 9) {
+      formated = (`${jmbg[0]}${jmbg[1]}` + "." + `${jmbg[2]}${jmbg[3]}` + "." + `1${jmbg[4]}${jmbg[5]}${jmbg[6]}.`);
+    } else {
+      formated = (`${jmbg[0]}${jmbg[1]}` + "." + `${jmbg[2]}${jmbg[3]}` + "." + `2${jmbg[4]}${jmbg[5]}${jmbg[6]}.`);
+    }
+  }
+  return formated
+}
+console.log(format(1201003123456)); 
