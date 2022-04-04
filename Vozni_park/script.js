2d033d34eb3ccf7e46c9fe365f4cabefe677f988
DoosanD <davicdusan@gmail.com>
2022-04-04 19:59:14 +0200

    novo

--- Vozni_park/script.js
+++ Vozni_park/script.js
@@ -1,24 +1,62 @@
-let kola = [];
+var kola = [];
+
+
 const dodajKola = (ev) => {
-  ev.preventDefault();
+  ev.preventDefault()
   let park = {
     tablice: document.getElementById("tablice").value,
     marka: document.getElementById("marka").value,
     godiste: document.getElementById("godiste").value,
   };
   kola.push(park);
+
+  showArray()
+
   document.forms[0].reset(); // izbrisi formu za sledeci unos
-  let pre = document.querySelector("#msg ol");
-  let li = "";
-  kola.forEach((k) => {
-    li += `
-    <li id='k'>
-    ${k.tablice}  ${k.marka} ${k.godiste}  <button onclick="parentNode.remove(); kola.splice(k,1)">Obrisi</button>
-      </li>
-    `;
-  });
-  pre.innerHTML = li;
-};
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("btn").addEventListener("click", dodajKola);
-});
+
+
+}
+
+function deleteItem(niz, index) {
+  niz.splice(index, 1);
+  showArray();
+}
+
+
+function showArray() {
+  document.querySelector("#msg ul").innerHTML = ''
+  for (let i = 0; i < kola.length; i++) {
+    document.querySelector("#msg ul").innerHTML += `
+        <li id=${i}>
+     ${i} ${kola[i].tablice}  ${kola[i].marka} ${kola[i].godiste}  <button onclick="deleteItem(kola,${i});console.log(kola);">Obrisi</button>
+        </li>
+        `
+  }
+}
+
+
+
+console.log('kola', kola)
+// }
+
+
+
+
+
+document.getElementById("btn").addEventListener("click", dodajKola);
+
+// window.onload = function () {
+//   document.querySelector("#l").onclick = function () {
+//     this.parentNode.parentNode.parentNode.removeChild(
+//       this.parentNode.parentNode
+//     );
+//     return false;
+//   };
+// };
+
+// <span
+//   id="close"
+//   onclick="this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode); return false;"
+// >
+//   x
+// </span>;
