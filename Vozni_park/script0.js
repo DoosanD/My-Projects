3ca77311849853637ab9578e3b63602e55811991
DoosanD <davicdusan@gmail.com>
2022-04-17 16:12:48 +0200

    Vozni park dorada

--- Vozni_park/script0.js
+++ Vozni_park/script0.js
@@ -1,7 +1,6 @@
 var kola = [];
-var kola2 = [];
-var kola3 = [];
 var park = {};
+var godina = new Date().getFullYear();
 
 const dodajKola = (ev) => {
   ev.preventDefault();
@@ -11,16 +10,8 @@ const dodajKola = (ev) => {
     godiste: document.getElementById("godiste").value,
     kilometraza: document.getElementById("kilometraza").value,
   };
-  if (park.kilometraza < 5000) {
-    kola.push(park);
-  } else if (park.kilometraza >= 5000 && park.kilometraza <= 15000) {
-    kola2.push(park);
-  } else if (park.kilometraza > 15000) {
-    kola3.push(park);
-  }
-
+  kola.push(park);
   showArray();
-
   document.forms[0].reset(); // izbrisi formu za sledeci unos
 };
 
@@ -33,23 +24,39 @@ function showArray() {
   document.querySelector("#msg ul").innerHTML = "<li id='1'></li>";
   document.querySelector("#msg2 ul").innerHTML = "<li id='2'></li>";
   document.querySelector("#msg3 ul").innerHTML = "<li id='3'></li>";
+
   for (let i = 0; i < kola.length; i++) {
-    if (document.getElementById("kilometraza").value <= 5000) {
+    if (
+      kola[i].godiste == "" ||
+      kola[i].kilometraza == "" ||
+      kola[i].marka == "" ||
+      kola[i].tablice == ""
+    ) {
+      alert("Unesite podatke");
+      location.reload();
+    }
+    if (kola[i].kilometraza <= 5000 && godina - kola[i].godiste <= 9) {
+      console.log("srednja", kola[i]);
       document.querySelector("#msg ul").innerHTML += `
         <li id=${i}>
      ${i} ${kola[i].tablice}  ${kola[i].marka} ${kola[i].godiste} ${kola[i].kilometraza}  <button onclick="deleteItem(kola,${i});console.log(kola);">Obrisi</button>
         </li>
         `;
-    } else if (
-      document.getElementById("kilometraza").value > 5000 &&
-      document.getElementById("kilometraza").value <= 15000
+    }
+    if (
+      // godina - kola[i].godiste > 20 ||
+      (kola[i].kilometraza > 5000 && kola[i].kilometraza <= 15000) ||
+      (godina - kola[i].godiste > 10 && godina - kola[i].godiste < 20)
     ) {
+      console.log("srednja", kola[i]);
       document.querySelector("#msg2 ul").innerHTML += `
       <li id=${i}>
    ${i} ${kola[i].tablice}  ${kola[i].marka} ${kola[i].godiste} ${kola[i].kilometraza}  <button onclick="deleteItem(kola,${i});console.log(kola);">Obrisi</button>
       </li>
       `;
-    } else if (document.getElementById("kilometraza").value > 15000) {
+    }
+    if (kola[i].kilometraza > 15000 || godina - kola[i].godiste > 30) {
+      console.log("stara", kola[i]);
       document.querySelector("#msg3 ul").innerHTML += `
       <li id=${i}>
    ${i} ${kola[i].tablice}  ${kola[i].marka} ${kola[i].godiste} ${kola[i].kilometraza}  <button onclick="deleteItem(kola,${i});console.log(kola);">Obrisi</button>
@@ -59,23 +66,4 @@ function showArray() {
   }
 }
 
-//console.log("kola", kola);
-// }
-
 document.getElementById("btn").addEventListener("click", dodajKola);
-
-// window.onload = function () {
-//   document.querySelector("#l").onclick = function () {
-//     this.parentNode.parentNode.parentNode.removeChild(
-//       this.parentNode.parentNode
-//     );
-//     return false;
-//   };
-// };
-
-// <span
-//   id="close"
-//   onclick="this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode); return false;"
-// >
-//   x
-// </span>;
