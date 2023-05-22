c216db295b40a264b2d220209297539c8c3b540e
DoosanD <davicdusan@gmail.com>
2023-05-22 20:14:46 +0200

    22-05-2023

---
+++ Wordpress_plugin/simple_contact_form/simple_contact_form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Simple Contact Form
+ * Descritpion: Simple Contact Form Tutorial
+ * Author: Dusan Davic
+ * Author URI: http://dusand.tk
+ * Version: 1.0.0
+ * Text Domain: simple_contact_form
+ * 
+ */
