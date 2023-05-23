44e229ad02a8f703a11e0cc1b9d80998f1d40ee2
DoosanD <davicdusan@gmail.com>
2023-05-23 21:31:38 +0200

    23-05-2023

--- Wordpress_plugin/simple_contact_form/simple_contact_form.php
+++ Wordpress_plugin/simple_contact_form/simple_contact_form.php
@@ -1,10 +1,32 @@
 <?php
+
 /**
  * Plugin Name: Simple Contact Form
- * Descritpion: Simple Contact Form Tutorial
+ * Description: Simple Contact Form Tutorial
  * Author: Dusan Davic
  * Author URI: http://dusand.tk
  * Version: 1.0.0
  * Text Domain: simple_contact_form
  * 
  */
+
+if (!defined('ABSPATH')) {
+
+   echo 'What are you looking for?';
+   exit;
+}
+
+class SIMPLE_CONTACT_FORM
+{
+   public function __construct()
+   {
+      add_action('init', array($this, 'CREATE_CUSTOM_POST_TYPE'));
+   }
+
+   public function CREATE_CUSTOM_POST_TYPE()
+   {
+      echo '<script>alert("ITS ALIVE")</script>';
+   }
+}
+
+new SIMPLE_CONTACT_FORM;
