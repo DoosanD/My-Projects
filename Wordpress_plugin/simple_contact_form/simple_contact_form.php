c2ee0c8033727a87dd369d717c2217fdce837c25
DoosanD <davicdusan@gmail.com>
2023-05-25 19:10:56 +0200

    25-05-2023

--- Wordpress_plugin/simple_contact_form/simple_contact_form.php
+++ Wordpress_plugin/simple_contact_form/simple_contact_form.php
@@ -5,7 +5,7 @@
  * Description: Simple Contact Form Tutorial
  * Author: Dusan Davic
  * Author URI: http://dusand.tk
- * Version: 1.0.0
+ * Version: 1.0.2
  * Text Domain: simple_contact_form
  * 
  */
@@ -20,7 +20,14 @@ class SIMPLE_CONTACT_FORM
 {
    public function __construct()
    {
+      // Create custom post type
       add_action('init', array($this, 'CREATE_CUSTOM_POST_TYPE'));
+
+      // Add assets (js, css, etc)
+      add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+
+      // Add Shortcode
+      add_shortcode('contact-form', array($this, 'load_shortcode'));
    }
 
    public function CREATE_CUSTOM_POST_TYPE()
@@ -43,6 +50,40 @@ class SIMPLE_CONTACT_FORM
 
       register_post_type('simple_contact_form', $args);
    }
+
+   public function load_assets()
+   {
+      wp_enqueue_style(
+         'simple-contact-form',
+         plugin_dir_url(__FILE__) . 'css/simple-contact-form.css',
+         array(),
+         1,
+         'all'
+      );
+
+      wp_enqueue_script(
+         'simple-contact-form',
+         plugin_dir_url(__FILE__) . 'js/simple-contact-form.js',
+         array('jquery'),
+         1,
+         true
+      );
+   }
+
+   public function load_shortcode()
+   { ?>
+      <div class="simple-contact-form">
+         <form action="" class="simple-contact-form__form">
+            <h2>Send us an Email</h2>
+            <p>Enter your information:</p>
+            <input name="name" type="text" placeholder="Name" class="form-control">
+            <input name="email" type="email" placeholder="Email" class="form-control">
+            <input name="number" type="tel" placeholder="Phone Number" class="form-control">
+            <textarea name="message" placeholder="Type your message" class="form-control"></textarea>
+            <button class="btn">Send Message!</button>
+         </form>
+      </div>
+<?php }
 }
 
 new SIMPLE_CONTACT_FORM;
