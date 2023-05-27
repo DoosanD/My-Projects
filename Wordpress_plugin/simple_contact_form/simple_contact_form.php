49183aef901b7ff9ee1525c7b08b66e299f8a697
DoosanD <davicdusan@gmail.com>
2023-05-27 19:28:44 +0200

    27-05-2023

--- Wordpress_plugin/simple_contact_form/simple_contact_form.php
+++ Wordpress_plugin/simple_contact_form/simple_contact_form.php
@@ -5,7 +5,7 @@
  * Description: Simple Contact Form Tutorial
  * Author: Dusan Davic
  * Author URI: https://www.linkedin.com/in/du%C5%A1an-davi%C4%87/
- * Version: 1.0.3
+ * Version: 1.0.4
  * Text Domain: simple_contact_form
  * 
  */
@@ -31,6 +31,9 @@ class SIMPLE_CONTACT_FORM
 
       // Load javascript
       add_action('wp_footer', array($this, 'load_scripts'));
+
+      // Register API
+      add_action('rest_api_init', array($this, 'register_rest_api'));
    }
 
    public function CREATE_CUSTOM_POST_TYPE()
@@ -76,14 +79,14 @@ class SIMPLE_CONTACT_FORM
    public function load_shortcode()
    { ?>
       <div class="simple-contact-form">
-         <form action="" class="simple-contact-form__form">
+         <form action="" id="simple-contact-form__form">
             <h2>Send us an Email</h2>
             <p>Enter your information:</p>
             <input name="name" type="text" placeholder="Name" class="form-control">
             <input name="email" type="email" placeholder="Email" class="form-control">
             <input name="number" type="tel" placeholder="Phone Number" class="form-control">
             <textarea name="message" placeholder="Type your message" class="form-control"></textarea>
-            <button class="btn">Send Message!</button>
+            <button type="submit" class="btn">Send Message!</button>
          </form>
       </div>
    <?php }
@@ -91,9 +94,40 @@ class SIMPLE_CONTACT_FORM
    public function load_scripts()
    { ?>
       <script>
-         alert("Does it work?");
+         var nonce = '<?php echo wp_create_nonce('wp_rest'); ?>';
+
+         (function($) {
+
+            $('#simple-contact-form__form').submit(function(event) {
+               event.preventDefault();
+               var form = $(this).serialize();
+               console.log(form);
+               $.ajax({
+                  method: 'post',
+                  url: '<?php echo get_rest_url(null, 'simple-contact-form/v1/send-email'); ?>',
+                  headers: {
+                     'X-WP-Nonce': nonce
+                  },
+                  data: form
+               })
+            });
+
+         })(jQuery);
       </script>
 <?php }
+
+   public function register_rest_api()
+   {
+      register_rest_route('simple-contact-form/v1', 'send-email', array(
+         'methods' => 'POST',
+         'callback' => array($this, 'handle_contact_form')
+      ));
+   }
+
+   public function handle_contact_form($data)
+   {
+      echo "hello this endpoint is working, i think...";
+   }
 }
 
 new SIMPLE_CONTACT_FORM;
